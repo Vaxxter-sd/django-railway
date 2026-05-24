b673ddb4346e5d76e48715ea1c11cc3ddf53171c
Vaxxter-sd <carlos213244@gmail.com>
2026-05-23 18:27:40 -0600

    models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class LeadFromFlask(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(max_length=100)
+    tipo_escuela = models.CharField(max_length=50)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        managed = False          # No crear la tabla en la BD
+        db_table = 'leads'       # Nombre exacto de la tabla en Clever Cloud
+        verbose_name = 'Lead desde Flask'
+        verbose_name_plural = 'Leads desde Flask'
+
+    def __str__(self):
+        return self.nombre
