bece057589279018ea12d813802fea6b74189215
Vaxxter-sd <carlos213244@gmail.com>
2026-05-23 18:28:17 -0600

    admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import LeadFromFlask
+
+@admin.register(LeadFromFlask)
+class LeadFromFlaskAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'correo', 'tipo_escuela', 'fecha_registro')
+    list_filter = ('tipo_escuela',)
+    search_fields = ('nombre', 'correo')
