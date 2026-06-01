acf52824a2bfb6318938c19cd595b9539b2d9fab
Vaxxter-sd <carlos213244@gmail.com>
2026-05-31 19:54:20 -0600

    models.py

--- home/models.py
+++ home/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class LeadFromFlask(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     correo = models.EmailField(max_length=100)
     tipo_escuela = models.CharField(max_length=50)
     fecha_registro = models.DateTimeField(auto_now_add=True)
-    estado = models.CharField(max_length=20, default='Nuevo')
-    notas = models.TextField(blank=True, null=True)
+    estado = models.CharField(max_length=20, default='Nuevo')   # ← nuevo
+    notas = models.TextField(blank=True, null=True)              # ← opcional
 
     class Meta:
         managed = False
