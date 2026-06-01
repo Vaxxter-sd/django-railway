ad88006bb61225215510bf33f5761dd62060c172
Vaxxter-sd <carlos213244@gmail.com>
2026-05-31 19:33:21 -0600

    admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import LeadFromFlask
+import csv
+from django.http import HttpResponse
 
-@admin.register(LeadFromFlask)
 class LeadFromFlaskAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'correo', 'tipo_escuela', 'fecha_registro')
-    list_filter = ('tipo_escuela',)
+    list_display = ('nombre', 'correo', 'tipo_escuela', 'fecha_registro', 'estado')
+    list_filter = ('estado', 'tipo_escuela')
     search_fields = ('nombre', 'correo')
+    list_editable = ('estado',)   # permite editar estado directamente en la lista
+    actions = ['exportar_a_csv']
+
+    def exportar_a_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="leads.csv"'
+        writer = csv.writer(response)
+        writer.writerow(['ID', 'Nombre', 'Correo', 'Tipo escuela', 'Fecha registro', 'Estado', 'Notas'])
+        for lead in queryset:
+            writer.writerow([lead.id, lead.nombre, lead.correo, lead.tipo_escuela,
+                             lead.fecha_registro.strftime('%Y-%m-%d %H:%M'), lead.estado, lead.notas])
+        return response
+    exportar_a_csv.short_description = "Exportar seleccionados a CSV"
+
+admin.site.register(LeadFromFlask, LeadFromFlaskAdmin)
