819b9b9f93acf4ca8b94f155cd4f195c4b2eb315
Vaxxter-sd <carlos213244@gmail.com>
2026-06-08 12:50:05 -0600

    urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from home import views
 from home.views import recibir_lead_api
 
 urlpatterns = [
-    path('', views.render_home, name='home')
+    path('', views.render_home, name='home'),
     path('api/recibir-lead/', recibir_lead_api, name='recibir_lead_api'),
 ]
