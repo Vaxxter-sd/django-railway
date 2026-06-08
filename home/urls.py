de6123646ada8b0b93a90eed157ee0c4b702c05b
Vaxxter-sd <carlos213244@gmail.com>
2026-06-08 12:39:46 -0600

    urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from home import views
+from home.views import recibir_lead_api
 
 urlpatterns = [
     path('', views.render_home, name='home')
+    path('api/recibir-lead/', recibir_lead_api, name='recibir_lead_api'),
 ]
